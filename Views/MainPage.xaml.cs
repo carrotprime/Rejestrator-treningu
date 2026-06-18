e274f988f05e77c8b37256d19d8b87f963bbb286
carrotprime <112853067+carrotprime@users.noreply.github.com>
2026-06-18 12:40:41 +0200

    Add files via upload

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using WorkoutApp.ViewModels;
+
+namespace WorkoutApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainPageViewModel _viewModel;
+
+        public MainPage(MainPageViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.LoadWorkoutsAsync();
+        }
+    }
+}
